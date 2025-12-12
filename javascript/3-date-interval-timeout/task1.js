b6ce12a6a8550315a3e5bad9493072bc2031787d
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-12-12 10:37:57 +0400

    date, interval, timeout

---
+++ javascript/3-date-interval-timeout/task1.js
@@ -0,0 +1,10 @@
+// 1-dən başlayaraq saniyələri sayan və hər növbəti 5-ə bölünən saniyədə "5!!!" çap edən proqram yazın.
+
+// Yuxarıda yazdığınız proqramın 11-ə çatdıqda dayanmasını təmin edin.
+
+// İşə salındıqda aşağıdakıları uyğun vaxtlarda çap edən proqram yazın. Öncəki addımlar üçün setInterval, son addım üçün setTimeout funksiyasından istifadə edin.
+// Proqram başladı. 3 saniyə gözləyin...
+// 3
+// 2
+// 1
+// Proqram sonlandı.
---
+++ javascript/3-date-interval-timeout/task2.js
@@ -0,0 +1,3 @@
+// Hazırkı tarixi və vaxtı hər saniyə konsola loq edən proqram yazın.
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
