d5b11a3a465b8bf20f186bd045be565c96293bfa
NureddinFarzaliyev <nureddin.fa@gmail.com>
2026-03-29 11:14:08 +0400

    OOP

---
+++ javascript/4-oop/task.js
@@ -0,0 +1,9 @@
+// Constructoru-u ad, soyad və yaş parametrləri götürən "Person" adlı class yaradın
+
+// Bu classa `getFullName` adında metod əlavə edin.
+
+// İstənilən parametr ilə bu classın instance-ı yaradın və `getFullName` metodunu çağırın.
+
+// Person classını extend edən "Employee" adlı class yaradın. Əsas parametrlərə əlavə olaraq `salary` və `position` parametrlərini də götürsün.
+
+// https://www.youtube.com/playlist?list=PL1PqvM2UQiMoGNTaxFMSK2cih633lpFKP
